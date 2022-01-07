a644707724b5c1e999eb7a7e1c1212907b8d5340
chamomilek <chamomileekk@gmail.com>
2022-01-08 01:26:51 +0300

    feat: used the principles of solid SR

--- script.rb
+++ script.rb
@@ -5,47 +5,70 @@ require 'uri'
 require 'net/http'
 require 'terminal-table'
 
-url = URI.parse('https://api.covid19api.com/summary')
+class HTTP
+  attr_reader :new_url
 
-https = Net::HTTP.new(url.host, url.port)
-https.use_ssl = true
+  def initialize(new_url)
+    @new_url = new_url
+  end
 
-request = Net::HTTP::Get.new(url)
-response = https.request(request)
-status = JSON.parse(response.body)['Countries']
-countries = status.map { |e| e['Country'] }
-rows = []
+  def proxy
+    url = URI.parse(new_url)
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = https.request(request)
+    status = JSON.parse(response.body)['Countries']
+  end
+
+  def cntrs(status)
+    countries = status.map { |e| e['Country'] }
+  end
+end
+
+class Output
+  attr_reader :aRGV, :countrie, :rows, :status
+
+  def initialize(aRGV, countrie, rows, status)
+    @aRGV = aRGV
+    @countrie = countrie
+    @rows = Array.new
+    @status = status
+  end
+
+  def letter
+    puts countrie if aRGV == 'C'
+  end
+
+  def c_name
+    (0...countrie.size).each do |index|
+      next unless aRGV == countrie[index]
+      # print status[index]
+      rows << ['Country', status[index]['Country']]
+      rows << ['CountryCode', status[index]['CountryCode']]
+      rows << ['Slug', status[index]['Slug']]
+      rows << ['NewConfirmed', status[index]['NewConfirmed']]
+      rows << ['TotalConfirmed', status[index]['TotalConfirmed']]
+      rows << ['NewDeaths', status[index]['NewDeaths']]
+      rows << ['TotalDeaths', status[index]['TotalDeaths']]
+      rows << ['NewRecovered', status[index]['NewRecovered']]
+      rows << ['TotalRecovered', status[index]['TotalRecovered']]
+      rows << ['Date', status[index]['Date']]
+      table = Terminal::Table.new title: 'Covid-19 Information', rows: rows
+      puts table
+    end
+  end
+end
+
+htp = HTTP.new 'https://api.covid19api.com/summary'
+# countrie = htp.proxy()
+status = htp.proxy
+countrie = htp.cntrs(status)
 
 puts 'Enter "C" to see the list of available countries'
 puts 'Or enter country name to see Covid informarion'
-ARGV[0] = gets.chomp
-
-puts countries if ARGV[0] == 'C'
-# if ARGV[0] == 'C'
-#   table = Terminal::Table.new do |t|
-#     t.headings = %w[id name]
-#      (0...countries.size).each do |index|
-#        t.rows[index] = [[index + 1], countries[index]]
-#      end
-#     t.style = { border_top: false, border_bottom: false }
-#   end
-#   puts table
-# end
-
-(0...countries.size).each do |index|
-  next unless ARGV[0] == countries[index]
-
-  # print status[index]
-  rows << ['Country', status[index]['Country']]
-  rows << ['CountryCode', status[index]['CountryCode']]
-  rows << ['Slug', status[index]['Slug']]
-  rows << ['NewConfirmed', status[index]['NewConfirmed']]
-  rows << ['TotalConfirmed', status[index]['TotalConfirmed']]
-  rows << ['NewDeaths', status[index]['NewDeaths']]
-  rows << ['TotalDeaths', status[index]['TotalDeaths']]
-  rows << ['NewRecovered', status[index]['NewRecovered']]
-  rows << ['TotalRecovered', status[index]['TotalRecovered']]
-  rows << ['Date', status[index]['Date']]
-  table = Terminal::Table.new title: 'Covid-19 Information', rows: rows
-  puts table
-end
+rows=[]
+aRGV = gets.chomp
+choice = Output.new(aRGV, countrie, rows, status)
+choice.letter
+choice.c_name
